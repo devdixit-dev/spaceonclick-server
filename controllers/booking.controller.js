f8ec105c60a77a47def988f8319004195c383fc6
Dev Dixit <devdixitsocial@gmail.com>
2025-10-23 15:17:06 +0530

    add : basic validations

--- controllers/booking.controller.js
+++ controllers/booking.controller.js
@@ -2,37 +2,47 @@ import Booking from "../models/booking.model.js";
 import Property from "../models/property.model.js";
 
 export const booking = async (request, response) => {
-  try{
-    const { 
-      propertyName, 
-      plan, 
-      date, 
-      time, 
-      firstName, 
-      lastName, 
-      email, 
-      contactNumber, 
-      companyName, 
-      details 
+  try {
+    const {
+      propertyName,
+      plan,
+      date,
+      time,
+      firstName,
+      lastName,
+      email,
+      contactNumber,
+      companyName,
+      details
     } = request.body;
 
+    if (!propertyName || !date || !time || !firstName || !lastName || !email || !contactNumber) {
+      return res.status(400).json({
+        success: false,
+        message: "Missing required booking details.",
+      });
+    }
+
     const property = await Property.findOne({ propertyName });
-    if(!property) {
+    if (!property) {
       return response.status(404).json({
         success: false,
         message: 'property not found or not available'
       });
     }
 
-    if(!property.isAvailable) {
+    if (!property.isAvailable) {
       return response.status(401).json({
         success: false,
         message: 'property is not available at this time'
       });
     }
 
-    const bookingExist = await Booking.findOne({ date, time });
-    if(bookingExist) {
+    const bookingExist = await Booking.findOne({
+      propertyID: property.propertyID, date, time
+    });
+
+    if (bookingExist) {
       return response.status(401).json({
         success: false,
         message: 'someone already booked this place at this time'
@@ -51,7 +61,7 @@ export const booking = async (request, response) => {
       data: booking
     });
   }
-  catch(err) {
+  catch (err) {
     console.log(`error in booking - ${err}`);
     return response.status(500).json({
       success: false,
